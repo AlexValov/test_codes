ec4f26119c9956910825352510aec085c743d523
alexsandrvalov <alexsandrvalov@gmail.com>
2021-01-17 14:02:46 +0200

    for test upload to histing

--- code_studio/settings.py
+++ code_studio/settings.py
@@ -126,8 +126,11 @@ USE_TZ = True
 # STATICFILES_DIRS = [STATIC_DIR, ]
 
 
+
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [STATIC_DIR, ]
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = '/media/'
