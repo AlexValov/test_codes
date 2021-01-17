71f87672b534c36e71d4ac44bb6636f48c7ea73b
alexsandrvalov <alexsandrvalov@gmail.com>
2021-01-17 16:39:55 +0200

    test static file

--- code_studio/settings.py
+++ code_studio/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 
 STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [STATIC_DIR, ]
+# STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
