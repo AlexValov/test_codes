0583b32fbaad9767839dd55131a28239bd9bee4f
alexsandrvalov <alexsandrvalov@gmail.com>
2021-01-17 16:17:13 +0200

    test static file

--- code_studio/settings.py
+++ code_studio/settings.py
@@ -123,12 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static") # Изначально пустой каталог, куда Django соберёт всё при выполнении manage.py collectstatic
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_dev"), # Каталог, куда вам нужно складывать статику проекта, не относящуюся к конкретному приложению
-]
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static") # Изначально пустой каталог, куда Django соберёт всё при выполнении manage.py collectstatic
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static_dev"), # Каталог, куда вам нужно складывать статику проекта, не относящуюся к конкретному приложению
+# ]
 
+STATIC_ROOT = "/staticfiles/static/"
+STATIC_URL = '/static/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
