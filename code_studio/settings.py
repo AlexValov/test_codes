c2a9fda98e1b379ce2544e6ddf90d10f0983bd4f
alexsandrvalov <alexsandrvalov@gmail.com>
2021-01-17 13:50:18 +0200

    for test upload to histing

--- code_studio/settings.py
+++ code_studio/settings.py
@@ -121,9 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_DIR = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [STATIC_DIR, ]
+
+
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = '/media/'
